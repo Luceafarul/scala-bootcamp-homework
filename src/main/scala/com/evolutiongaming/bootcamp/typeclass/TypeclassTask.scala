bfc22000f95ee775a43612615a43a275daf7d521
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-02-19 00:52:40 +0200

    Add solution for first task for type-classes homework

---
+++ src/main/scala/com/evolutiongaming/bootcamp/typeclass/TypeclassTask.scala
@@ -0,0 +1,34 @@
+package com.evolutiongaming.bootcamp.typeclass
+
+object TypeclassTask extends App {
+  // Create type class -> class with type parameter
+  // and pass type to method param
+  trait HashCode[T] {
+    def hash(x: T): Int
+  }
+
+  // Summoner implementation:
+  // apply method with implicit type-class param
+  // return implicit param
+  object HashCode {
+    def apply[T](implicit instance: HashCode[T]): HashCode[T] = instance
+  }
+
+  // Add syntax:
+  // type as constructor argument
+  // method name == syntax
+  // implicit type-class as method argument
+  // when have implicit param for type-class then call method from type-class with constructor arg
+  implicit class HashCodeSyntax[A](x: A) {
+    def hash(implicit instance: HashCode[A]): Int =
+      instance.hash(x)
+  }
+
+  // Define implicit type-class instance for String
+  implicit val hashLikeString: HashCode[String] = x => x.hashCode
+
+  // TODO: make an instance for String
+  // TODO: write "abc".hash to check everything
+  val res = "abc".hash
+  println(res)
+}
